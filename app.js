f0e2e55d62d23f19c6ad139a32a970c40bbaf58f
kawzar <hmacarena@gmail.com>
2021-01-04 16:01:38 -0300

    fixes

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist") + "/index.html");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log("Starting server at port " + port);
 });
